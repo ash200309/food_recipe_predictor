6c3389e9da4a088d95db5526fdd2d9a6869e8ce5
ansh <124485267+ash200309@users.noreply.github.com>
2024-02-15 15:25:03 +0530

    Update multihead_attention.py

--- src/modules/multihead_attention.py
+++ src/modules/multihead_attention.py
@@ -118,7 +118,7 @@ class MultiheadAttention(nn.Module):
         if key_padding_mask is not None:
             # don't attend to padding symbols
             attn_weights = attn_weights.view(bsz, self.num_heads, tgt_len, src_len)
-            attn_weights = attn_weights.float().masked_fill(
+            attn_weights = attn_weights.float().masked_fill_(
                 key_padding_mask.unsqueeze(1).unsqueeze(2),
                 float('-inf'),
             ).type_as(attn_weights)  # FP16 support: cast to float and back
